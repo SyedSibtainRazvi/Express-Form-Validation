2464105e59fcef7b098a1bbf107831a19de872c9
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-11-04 22:24:31 +0530

    fix: prettier issue

--- index.js
+++ index.js
@@ -1,55 +1,45 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const bodyparser = require("body-parser")
-const path = require("path")
+const bodyparser = require("body-parser");
+const path = require("path");
 
-const { body, validationResult } = require("express-validator")
+const { body, validationResult } = require("express-validator");
 
-const port = process.env.port || 3000
+const port = process.env.port || 3000;
 
 // Setting up View Engine for test purposes
 
-app.set("views", path.join(__dirname))
-app.set("view engine", "ejs")
+app.set("views", path.join(__dirname));
+app.set("view engine", "ejs");
 
 // Setting up middleware
-app.use(bodyparser.urlencoded({ extended: false }))
-app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
 app.get("/", function (req, res) {
-    res.render("Form")
-})
+  res.render("Form");
+});
 
 const validateStandup = [
-    body("title", "titile shuld have length").isLength({ min: 5, max: 20 }),
-    body("questions", "question should not be empty").not().isEmpty(),
-    body("channel", "channel should not be empty").not().isEmpty(),
-    body("members", "members should have length").isLength({ min: 5, max: 20 }),
-]
+  body("title", "titile shuld have length").isLength({ min: 5, max: 20 }),
+  body("questions", "question should not be empty").not().isEmpty(),
+  body("channel", "channel should not be empty").not().isEmpty(),
+  body("members", "members should have length").isLength({ min: 5, max: 20 }),
+];
 
-app.post("/validate", [validateStandup]
-), (req, res) => {
-    const errors = validationResult(req)
-    console.log("Hello");
+app.post("/validate", [validateStandup]),
+  (req, res) => {
+    const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        return res.status(404).json(errors)
+      return res.status(404).json(errors);
+    } else {
+      res.status(200).send("Form Validated Successfully");
     }
-    else {
-        res.status(200).send("Form Validated Successfully")
-    }
-}
+  };
 
 app.listen(port, function (error) {
-    if (error) throw error
-    console.log("Server", port)
-})
-
-// app.post("/validate", (req,res) => {
-// res.send("Hello")
-// })
-
-// app.listen(port, () => {
-//     console.log(`App listening to port ${port}`);
-// })
+  if (error) throw error;
+  console.log("Server", port);
+});
